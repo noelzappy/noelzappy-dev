d25f8c1b55a6cb713a194ac41c82809ea4b79d79
Emmanuel Yeboah <noelzappy@gmail.com>
2025-12-29 01:19:56 +0000

    fix: build error and accessibility warning

--- src/routes/page.svelte.spec.ts
+++ src/routes/page.svelte.spec.ts
@@ -5,7 +5,14 @@ import Page from './+page.svelte';
 
 describe('/+page.svelte', () => {
 	it('should render h1', async () => {
-		render(Page);
+		render(Page, {
+			data: {
+				streamed: {
+					posts: Promise.resolve([]),
+					notes: Promise.resolve([])
+				}
+			}
+		});
 
 		const heading = page.getByRole('heading', { level: 1 });
 		await expect.element(heading).toBeInTheDocument();
