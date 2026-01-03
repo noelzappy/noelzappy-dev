f61b7e42994168ffe6314f6473d0a0f2b41caab6
Emmanuel Yeboah <noelzappy@gmail.com>
2026-01-03 20:50:22 +0000

    chore: updated project types

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -5,6 +5,22 @@ export interface ProjectFrontmatter {
 	featured: boolean;
 	publishedAt: string;
 	featuredImage?: string;
+	liveUrl?: string;
+	iosUrl?: string;
+	androidUrl?: string;
+	github?: string;
+	role: string;
+	team?: string; // 4 Members (1 PM, 2 Developers, 1 Designer, etc)
+	status: string; // Shipped, In Progress, Cancelled, Archived
+	client?: string;
+	gallery?: string[];
+	featuredStack?: string[];
+	stack?: string[];
+	stats?: {
+		key: string;
+		value: string;
+		description?: string;
+	}[];
 }
 
 export interface ProjectData extends ProjectFrontmatter {
