87a51956f4c79b16bc0d2cef04580942a633bb56
Emmanuel Yeboah <noelzappy@gmail.com>
2026-01-04 00:12:39 +0000

    feat: refactor load function to retrieve GitHub access token from platform environment

--- src/routes/open-source/+page.server.ts
+++ src/routes/open-source/+page.server.ts
@@ -1,9 +1,9 @@
 import type { PageServerLoad } from './$types';
-import { GITHUB_ACCESS_TOKEN } from '$env/static/private';
 import { fetchSelectedGithubRepos, fetchContributions } from '$lib/integrations/github';
 import { GITHUB_USERNAME } from '$lib/shared/constants';
 
-export const load: PageServerLoad = async () => {
+export const load: PageServerLoad = async ({ platform }) => {
+	const GITHUB_ACCESS_TOKEN = platform?.env?.GITHUB_ACCESS_TOKEN;
 	try {
 		const [repos, contributions] = await Promise.all([
 			fetchSelectedGithubRepos(GITHUB_ACCESS_TOKEN),
